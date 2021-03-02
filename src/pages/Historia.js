aaed20b70efc2b6e651f533952224675dbd83dba
caroline birgersson <caroline@birgerochblom.se>
2021-03-02 16:40:11 +0100

    Historia

--- src/pages/Historia.js
+++ src/pages/Historia.js
@@ -2,17 +2,24 @@ import React from 'react'
 
 export const Historia = () => {
   return (
-    <div>
-      <h1>Akademins historia</h1>
-      <p>
-        Bild och Ord Akademin vill motverka specialisering och fragmentisering. 
-        Den ska ge uppmuntran och stöd åt dagens informatörer och bidra till att förbättra utbildningen av morgondagens. Akademin har minst 20 och högst 55 ledamöter. 
-        Till akademin är anslutet ett akademiråd med stödjande och rådgivande funktioner.
-
-        Sven Lidman började sin yrkeskarriär hos Natur & Kulturs grundare Johan Hansson. Det blev början på ett omfattande förnyelsearbete med det som skulle komma att bli
-        Sven Lidmans stora insats som producent: det moderna bildlexikonet. 
-        I de flesta svenska hem finns ett uppslagsverk som Sven Lidman ligger bakom och det totala antalet sålda exemplar, inklusive de utländska utgåvorna, uppgår idag till 25 miljoner volymer.</p>
-    </div>
+    <section className="historiaWrapper">
+      <article className="akademinsUppdrag">
+        <h1>Akademins uppdrag</h1>
+        <p>
+          Bild och Ord Akademin är ett tvärvetenskapliga forum för olika yrkesgrupper inom informationsområdet, med uppgift att motverka specialisering och fragmentisering. 
+          Akademin ska uppmuntra och ge stöd åt dagens informatörer, samtidigt som den ska förbättra utbildningen av morgondagens.
+        </p>
+      </article>
+      <article className="historia">
+        <h3>Sven Lidman – akademins grundare</h3>
+        <p>
+          Initiativet till Bild och Ord Akademin togs av <a href="https://sv.wikipedia.org/wiki/Sven_Lidman_(lexikograf)">Sven Lidman</a> år 1983.  
+          Han började sin yrkeskarriär hos Natur & Kulturs grundare Johan Hansson. Det blev början på ett omfattande förnyelsearbete med det som skulle komma att bli
+          Sven Lidmans stora insats som producent: det moderna bildlexikonet. 
+          I de flesta svenska hem finns ett uppslagsverk som Sven Lidman ligger bakom och det totala antalet sålda exemplar, inklusive de utländska utgåvorna, uppgår idag till 25 miljoner volymer.
+        </p>
+      </article>
+    </section>
   )
 }
 
--- src/pages/Startsida.js
+++ src/pages/Startsida.js
@@ -6,7 +6,7 @@ export const Startsida = () => {
     <>
     <section className="uppdrag">
       <h2>Akademins uppdrag</h2>
-        <p>Bild och Ord Akademi är en informationsakademi, som grundades 1983 på initiativ av lexikografen Sven Lidman för att möta ett upplevt behov av en institution som "vårdar språket och kreativiteten i ord- och bildkommunikationen".</p>
+        <p>Bild och Ord Akademin är en informationsakademi, som grundades 1983 på initiativ av lexikografen Sven Lidman för att möta ett upplevt behov av en institution som "vårdar språket och kreativiteten i ord- och bildkommunikationen".</p>
     </section>
     <section className="pristagare">
       <h2>Pristagare</h2>
