b9c92cfd2ef1cd7c29c740a33f5747d1cade5842
Moa Blomkvist <moa@birgerochblom.se>
2021-03-02 15:22:35 +0100

    assets

--- src/pages/Startsida.js
+++ src/pages/Startsida.js
@@ -3,6 +3,8 @@ import React from 'react'
 
 export const Startsida = () => {
   return (
-    <></>
+    <section className="uppdrag">
+      <p>Bild och Ord Akademi är en informationsakademi, som grundades 1983 på initiativ av lexikografen Sven Lidman för att möta ett upplevt behov av en institution som "vårdar språket och kreativiteten i ord- och bildkommunikationen". Initiativet till Bild och Ord Akademien, som är ett tvärvetenskapligt forum för olika yrkesgrupper inom informationsområdet, togs av Sven Lidman år 1983.</p>
+    </section>
   )
 }
