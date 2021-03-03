576542ef2260e6bd74db2a3747bd386e8941bc6d
caroline birgersson <caroline@birgerochblom.se>
2021-03-03 11:34:23 +0100

    fixed indentation

--- src/pages/Startsida.js
+++ src/pages/Startsida.js
@@ -6,29 +6,29 @@ export const Startsida = () => {
     <section className="start-container">
       <article className="uppdrag">
         <h2>Akademins uppdrag</h2>
-          <p>Bild och Ord Akademin är en informationsakademi, som grundades 1983 på initiativ av lexikografen Sven Lidman för att möta ett upplevt behov av en institution som "vårdar språket och kreativiteten i ord- och bildkommunikationen".</p>
-          <Link to="/historia" className="white-arrow-link-element">
-            <img className="arrow" src="assets/white-right-arrow.svg" alt="arrow-link"/>
-            <p>Akademins historia</p>
-          </Link>
+        <p>Bild och Ord Akademin är en informationsakademi, som grundades 1983 på initiativ av lexikografen Sven Lidman för att möta ett upplevt behov av en institution som "vårdar språket och kreativiteten i ord- och bildkommunikationen".</p>
+        <Link to="/historia" className="white-arrow-link-element">
+          <img className="arrow" src="assets/white-right-arrow.svg" alt="arrow-link"/>
+          <p>Akademins historia</p>
+        </Link>
       </article>
 
       <article className="pristagare">
         <h2>Pristagare</h2>
-          <h3>Bibi Rödöö</h3>
-          <h3>Anders Hansen</h3>
-          <Link to="/priser" className="black-arrow-link-element">
-            <img className="arrow" src="assets/black-right-arrow.svg" alt="arrow-link"/>
-            <p>Tidigare priser</p>
-          </Link>
+        <h3>Bibi Rödöö</h3>
+        <h3>Anders Hansen</h3>
+        <Link to="/priser" className="black-arrow-link-element">
+          <img className="arrow" src="assets/black-right-arrow.svg" alt="arrow-link"/>
+          <p>Tidigare priser</p>
+        </Link>
       </article>
 
       <article className="ledamoter">
         <h2>Ledamöter</h2>
-          <Link to="/ledamöter" className="white-arrow-link-element">
-            <img className="arrow" src="assets/white-right-arrow.svg" alt="arrow-link"/>
-            <p>Akademins stolar</p>
-          </Link>
+        <Link to="/ledamöter" className="white-arrow-link-element">
+          <img className="arrow" src="assets/white-right-arrow.svg" alt="arrow-link"/>
+          <p>Akademins stolar</p>
+        </Link>
       </article>
     </section>
   )
