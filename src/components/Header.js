a86571d1cb85ba11006b85a6195cf0484b80ae6d
caroline birgersson <caroline@birgerochblom.se>
2021-03-03 15:28:35 +0100

    added a link to the header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <section className="header">
-      <img className="logo" src="./assets/BOA.png" alt="logo"/>
-      <p className="sub-heading">Bild & Ord Akadmin</p>
+      <Link to ="/">
+        <img className="logo" src="./assets/BOA.png" alt="logo"/>
+      </Link>
+      <p className="sub-heading">Bild & Ord Akademin</p>
     </section>
   )
 }
