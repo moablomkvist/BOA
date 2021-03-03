3f3cdf305743ca67d1c1421875d80d0927458644
Moa Blomkvist <moa@birgerochblom.se>
2021-03-03 11:38:06 +0100

    handle error

--- src/pages/Priser.js
+++ src/pages/Priser.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
 
-export const Priser = () => {
-  const [active, setActive] = useState(0);
+// export const Priser = () => {
+//   const [active, setActive] = useState(0);
   
-  let indexPlus;
+//   let indexPlus;
 
-  const eventHandler = (e, index) => {
-    e.preventDefault();
-    setActive(index)
-  };
+//   const eventHandler = (e, index) => {
+//     e.preventDefault();
+//     setActive(index)
+//   };
 
-  const IndexCount = (index) => {
-    indexPlus = index + 1;
-    return indexPlus
-  }
+//   const IndexCount = (index) => {
+//     indexPlus = index + 1;
+//     return indexPlus
+//   }
 
 
-  return (
-    <>
-      <button onClick={}>Lidmanpriset</button>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <button onClick={}>Lidmanpriset</button>
+//     </>
+//   )
+// }
