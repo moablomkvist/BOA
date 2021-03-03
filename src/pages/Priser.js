f2dc9ac296daa2117163a9683a875bfbe914cffd
Moa Blomkvist <moa@birgerochblom.se>
2021-03-03 14:14:25 +0100

    accordian

--- src/pages/Priser.js
+++ src/pages/Priser.js
@@ -1,25 +1,45 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react';
+import data from '../data.json';
 
 
-// export const Priser = () => {
-//   const [active, setActive] = useState(0);
+export const Priser = () => {
+const [active, setActive] = useState(0);
   
-//   let indexPlus;
+let indexPlus;
 
-//   const eventHandler = (e, index) => {
-//     e.preventDefault();
-//     setActive(index)
-//   };
+const eventHandler = (e, index) => {
+  e.preventDefault();
+  setActive(index)
+  };
 
-//   const IndexCount = (index) => {
-//     indexPlus = index + 1;
-//     return indexPlus
-//   }
+const IndexCount = (index) => {
+indexPlus = index + 1;
+  return indexPlus
+}
 
 
-//   return (
-//     <>
-//       <button onClick={}>Lidmanpriset</button>
-//     </>
-//   )
-// }
+return (
+  <div>
+    <form>
+      { data.map((tab, index) => (
+        <div key={index}> 
+        <h3>
+        <button 
+          onClick={(e) => eventHandler(e, index)}
+          className={ active === index ? 'active' : 'inactive' }
+          aria-expanded={ active === index ? 'true' : 'false' }
+          aria-controls= { 'sect-' + IndexCount(index) }
+          aria-disabled= { active === index ? 'true' : 'false' }
+          tabIndex={IndexCount(index)}
+        > 
+        <span className="title-wrapper">{tab.title}
+          <span className={ active === index ? 'plus' : 'minus'}></span>
+        </span>
+        </button>
+        </h3>
+    </div>
+    ))}
+    </form>
+  </div>
+)
+}
