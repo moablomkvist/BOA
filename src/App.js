a4b9f30c123c29f6923b613b82490f1542b2548f
caroline birgersson <caroline@birgerochblom.se>
2021-03-03 10:57:38 +0100

    created links with react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import { Startsida } from 'pages/Startsida'
+import { Historia } from 'pages/Historia'
+import { Ledamoter } from 'pages/Ledamoter'
+import { Priser } from 'pages/Priser'
 
 export const App = () => {
   return (
-    <div>
-      <Startsida />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+
+          <Route path="/" exact>
+            <Startsida />
+          </Route>
+
+          <Route path="/historia" exact>
+            <Historia />
+          </Route>
+
+          <Route path="/ledamöter" exact>
+            <Ledamoter />
+          </Route>
+
+          <Route path="/priser" exact>
+            <Priser />
+          </Route>
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
--- src/pages/Startsida.js
+++ src/pages/Startsida.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import { Header } from 'components/Header'
 
@@ -11,24 +12,32 @@ export const Startsida = () => {
         <h2>Akademins uppdrag</h2>
           <p>Bild och Ord Akademi är en informationsakademi, som grundades 1983 på initiativ av lexikografen Sven Lidman för att möta ett upplevt behov av en institution som "vårdar språket och kreativiteten i ord- och bildkommunikationen".</p>
         <div className="link-element">
-          <img className="arrow" src="assets/white-right-arrow.svg" alt="arrow-link"></img>
-          <p>Akademins historia</p>
+          <Link to="/historia">
+            <img className="arrow" src="assets/white-right-arrow.svg" alt="arrow-link"></img>
+            <p>Akademins historia</p>
+          </Link>
         </div>
       </article>
+
       <article className="pristagare">
         <h2>Pristagare</h2>
           <h3>Bibi Rödöö</h3>
           <h3>Anders Hansen</h3>
         <div className="link-element">
-          <img className="arrow" src="assets/black-right-arrow.svg" alt="arrow-link"></img>
-          <p>Tidigare priser</p>
+          <Link to="/priser">
+            <img className="arrow" src="assets/black-right-arrow.svg" alt="arrow-link"></img>
+            <p>Tidigare priser</p>
+          </Link>
         </div>
       </article>
+
       <article className="ledamoter">
         <h2>Ledamöter</h2>
         <div className="link-element">
-          <img className="arrow" src="assets/white-right-arrow.svg" alt="arrow-link"></img>
-          <p>Akademins stolar</p>
+          <Link to="/ledamöter">
+            <img className="arrow" src="assets/white-right-arrow.svg" alt="arrow-link"></img>
+            <p>Akademins stolar</p>
+          </Link>
         </div>
       </article>
     </section>
