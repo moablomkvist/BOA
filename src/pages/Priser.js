58720bad4b12ce3b03305b689cdb1a668fd1ccb5
caroline birgersson <caroline@birgerochblom.se>
2021-03-04 15:47:40 +0100

    merge

--- src/pages/Priser.js
+++ src/pages/Priser.js
@@ -1,24 +1,29 @@
-import React, { useState } from 'react';
-import data from '../data.json';
+import React, { useState } from "react";
+import data from "../data.json";
 
 export const Priser = () => {
-const [isOpen, setOpen] = useState(false);
-  
-return (
-  <section className="accordion-wrapper">
-    <div className={`accordion-title ${isOpen ? "open" : ""}`}
-      onClick={() => setOpen(!isOpen)}>
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <section className="accordion-wrapper">
+      <div
+        className={`accordion-title ${isOpen ? "open" : ""}`}
+        onClick={() => setOpen(!isOpen)}
+      >
         <h3 className="lidman-rubrik">Lidmanpriset</h3>
         <div className={`accordion-item ${!isOpen ? "collapsed" : ""}`}>
-  {data.priser.lidmanpriset.map(lidmanpriset => { 
-    return (
-      <section key={lidmanpriset.year}>
-      <p className="accordion-content" > {lidmanpriset.year} {lidmanpriset.pristagare}</p>
-      </section>
-      )
-  })} 
-  </div>
-  </div>
-  </section>
-)}
-
+          {data.priser.lidmanpriset.map((lidmanpriset) => {
+            return (
+              <section key={lidmanpriset.year}>
+                <p className="accordion-content">
+                  {" "}
+                  {lidmanpriset.year} {lidmanpriset.pristagare}
+                </p>
+              </section>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
