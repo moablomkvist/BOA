abf75d0f535ce5619c8e57fca28116e189ed8ee8
caroline birgersson <caroline@birgerochblom.se>
2021-03-04 15:40:15 +0100

    merge

--- src/pages/Priser.js
+++ src/pages/Priser.js
@@ -1,27 +1,10 @@
 import React, { useState } from 'react';
 import data from '../data.json';
 
-
 export const Priser = () => {
-const [active, setActive] = useState(false);
-  
-
-
-const eventHandler = (event) => {
-  event.preventDefault();
-  setActive(true)
-  };
-
 
 return (
   <section>
-  {data.priser.lidmanpriset.map(lidmanpriset => { 
-    return (
-      <>
-      <p>{lidmanpriset.year}</p>
-      <p>{lidmanpriset.pristagare}</p>
-      </>
-      )
-  })} 
+    
   </section>
 )}
