96a494673dd50ebc7c77460c96eb6e0184549d70
Moa Blomkvist <moa@birgerochblom.se>
2021-03-04 13:59:33 +0100

    map

--- src/pages/Priser.js
+++ src/pages/Priser.js
@@ -3,43 +3,25 @@ import data from '../data.json';
 
 
 export const Priser = () => {
-const [active, setActive] = useState(0);
+const [active, setActive] = useState(false);
   
-let indexPlus;
 
-const eventHandler = (e, index) => {
-  e.preventDefault();
-  setActive(index)
+
+const eventHandler = (event) => {
+  event.preventDefault();
+  setActive(true)
   };
-
-const IndexCount = (index) => {
-indexPlus = index + 1;
-  return indexPlus
-}
 
 
 return (
-  <div>
-    <form>
-      { data.map((tab, index) => (
-        <div key={index}> 
-        <h3>
-        <button 
-          onClick={(e) => eventHandler(e, index)}
-          className={ active === index ? 'active' : 'inactive' }
-          aria-expanded={ active === index ? 'true' : 'false' }
-          aria-controls= { 'sect-' + IndexCount(index) }
-          aria-disabled= { active === index ? 'true' : 'false' }
-          tabIndex={IndexCount(index)}
-        > 
-        <span className="title-wrapper">{tab.title}
-          <span className={ active === index ? 'plus' : 'minus'}></span>
-        </span>
-        </button>
-        </h3>
-    </div>
-    ))}
-    </form>
-  </div>
-)
-}
+  <section>
+  {data.priser.lidmanpriset.map(lidmanpriset => { 
+    return (
+      <>
+      <p>{lidmanpriset.year}</p>
+      <p>{lidmanpriset.pristagare}</p>
+      </>
+      )
+  })} 
+  </section>
+)}
