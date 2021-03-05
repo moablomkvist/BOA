03d21c991232bc46f4273f328af5d4b7208647aa
caroline birgersson <caroline@birgerochblom.se>
2021-03-05 14:57:46 +0100

    added texts

--- src/pages/Ledamoter.js
+++ src/pages/Ledamoter.js
@@ -1,8 +1,59 @@
 import React from 'react'
 
+import { TillbakaKnapp } from 'components/TillbakaKnapp'
 
 export const Ledamoter = () => {
   return(
-    <></>
-  )
+    <section className="ledamöter-wrapper">
+    <TillbakaKnapp />
+    <article className="akademins-stolar">
+      <h1>Akademins stolar</h1>
+      <p>
+        I akademin sitter välrenommerade personer inom olika kreativa verksamhetsområden.
+        Journalister, copywriters och redaktörer.
+        Arkitekter, konstnärer och fotografer. 
+        Producenter, illustratörer och filmregissörer.
+        Hantverken är spridda men det som förenar är att alla bidrar till ett skapande inom bild eller ord.
+      </p>
+    </article>
+    <article className="listade-ledamöter">
+      <h3>Invalda ledamöter</h3>
+      <ul>
+        <li>Tom Alandh, journalist och dokumentärfilmare</li>
+        <li>Jan-Erik Ander, tecknare och creative director</li>
+        <li>Solbritt Benneth, arkeolog</li>
+        <li>Bo Bergström, art director</li>
+        <li>Caroline Birgersson, copywriter och utbildare</li>
+        <li>Moa Blomkvist, copywriter och utbildare</li>
+        <li>Anna Clarén, fotograf</li>
+        <li>Kristin Dahl, filosofie doktor</li>
+        <li>Anders Engman, fotograf</li>
+        <li>Bobo Engström, arkitekt</li>
+        <li>Maria Eriksson, filmregissör</li>
+        <li>Björn Fjaestad, publicist</li>
+        <li>Marcus Gärde, grafisk designer</li>
+        <li>Catharina Grünbaum, filosofie hedersdoktor och språkvårdare</li>
+        <li>Bengt Gullbring, art director</li>
+        <li>Owe Gustafson, illustratör och filmtecknare</li>
+        <li>Anders Hanser, fotograf och filmare</li>
+        <li>Sture Hegefors, tecknare</li>
+        <li>Cecilia Johansson, planner</li>
+        <li>Peder Jonsson, teknologie doktor</li>
+        <li>Björn Karlsson, redaktör</li>
+        <li>Christina Knight, creative director</li>
+        <li>Carolina Laudon, typsnittstecknare & art director</li>
+        <li>Inga Lewenhaupt, filosofie doktor och teater- och musikforskare</li>
+        <li>Krister Nathanaelson, tv-producent</li>
+        <li>Elisabeth Reslegård, projektledare</li>
+        <li>Annika Röhl, grafisk redaktör</li>
+        <li>Anna Schytt, vetenskapsjournalist</li>
+        <li>Georg Sessler, konstnär</li>
+        <li>Richard Wahlund, professor</li>
+        <li>Bengt Wanselius, fotograf och filmare</li>
+        <li>Marianne von Baumgarten-Lindberg, journalist och förläggare</li>
+        <li>Torgny Wärn, journalist och tecknare</li>
+      </ul>
+    </article>
+  </section>
+)
 }
--- src/pages/Priser.js
+++ src/pages/Priser.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React from 'react';
 
 import { Lidman } from 'components/Lidman'
 import { Encyklopedi } from 'components/Encyklopedi'
-
+import { TillbakaKnapp } from 'components/TillbakaKnapp'
 
 export const Priser = () => {
   return (
     <>
-      <Lidman />
-      <Encyklopedi />
+      <TillbakaKnapp />
+      <article className="akademins-priser">
+        <h1>Priser</h1>
+        <p>
+          Ända sedan 1980-talet har våra två priser delats ut till pristagare av bred rang. 
+          På scen har det stått allt ifrån barnboksförfattare, psykiatriker och dokumentärfilmare till programchefer, professorer och modellmakare. 
+          Gemensamt har varit att de bidragit till att berika med bild och ord.
+        </p>
+      </article>
+
+      <article className="pristagare">
+        <h3>Vinnare av Lidmanpriset</h3>
+        <p>
+          Som belönar god information i ord och bild.
+        </p>
+        <Lidman />
+        <h3>Vinnare av Ann-Marie Lunds Encyklopedipris</h3>
+        <p>
+          För folkbildande insatser och ett populärt berättande av encyklopedisk art och bredd.  
+        </p>
+        <Encyklopedi />
+      </article>
     </>
   )
 }
